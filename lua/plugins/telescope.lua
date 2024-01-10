047e4407531987be9185c0bf0457799bd8f60166
Łukasz Matysiak <lukasz.1.matysiak@gmail.com>
2024-01-10 07:57:45 +0100

    Add telescope.lua The keymap is set to override the default LazyVim behavior. It automatically searches for files with git ls-files --others. This leaves out files located in git submodules. Setting opts.pickers.git_files.recurse_submodules to false causes an error due to both it and others being set to true. I haven't been able to solve it yet, so I opted for the keymap

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {"<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find files"}
+    },
+    -- opts = {
+    --   pickers = {
+    --     git_files = {
+    --       others = false,
+    --       recurse_submodules = true
+    --     }
+    --   }
+    -- }
+  }
+}
