003177f830f12a96a041d124cbab07b91419bbfc
Łukasz Matysiak <lukasz.1.matysiak@gmail.com>
2024-01-10 08:01:48 +0100

    Add custom snippets to luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,6 +4,7 @@ return {
     "rafamadriz/friendly-snippets",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/my_snippets/" })
     end,
   },
 }
