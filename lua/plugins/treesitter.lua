871f4d9fb321aa87da3421559821a5d81da8836e
Łukasz Matysiak <lukasz.1.matysiak@gmail.com>
2024-01-10 07:56:44 +0100

    Add treesitter.lua

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+    {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rust",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+
+}
