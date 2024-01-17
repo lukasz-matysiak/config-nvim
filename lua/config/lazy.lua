ff7928fb1ac79c291bd95a15885a063f596dbf54
Łukasz Matysiak <lukasz.1.matysiak@gmail.com>
2024-01-17 22:18:48 +0100

    Enable cmake extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "plugins" },
   },
   defaults = {
