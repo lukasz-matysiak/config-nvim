b835be6540a9ed5a71ecd39d1d84b19ffe9a4bbc
Łukasz Matysiak <lukasz.1.matysiak@gmail.com>
2024-03-20 08:33:27 +0100

    User // to comment in qmljs

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -6,6 +6,8 @@ return {
       custom_commentstring = function()
         if vim.bo.filetype == "cpp" then
             return "// %s"
+        elseif vim.bo.filetype == "qmljs" then
+            return "// %s"
         else
             return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
         end
