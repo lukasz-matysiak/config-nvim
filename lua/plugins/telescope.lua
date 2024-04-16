1b28cf5fd311790cb934bcab8ff2308b268bc1d6
Łukasz Matysiak <lukasz.1.matysiak@gmail.com>
2024-04-16 17:04:52 +0200

    Add telescope-live-grep-args.nvim
    By default live grep does not accept any arguments.
    This makes searching specific directories or filetypes hard.
    
    Add telescope-live-grep-args.nvim and bind <leader>sg to it

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+        {
+            "nvim-telescope/telescope-live-grep-args.nvim" ,
+            -- This will not install any breaking changes.
+            -- For major updates, this must be adjusted manually.
+            --   version = "^1.0.0",
+        },
+    },
     keys = {
-      {"<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find files"}
+      {"<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find files"},
+      {"<leader>sg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Grep files with args"}
     },
     opts = {
         pickers = {
@@ -20,6 +29,9 @@ return {
                 -- follow = true,
                 -- hidden = true
             }
+        },
+        extensions = {
+            live_grep_args = {}
         }
     }
     -- opts = {
